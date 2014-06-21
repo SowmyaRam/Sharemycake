59b70fef77bbe4a12690cd2cd6e38479bba66f63
SowmyaRam <sowmyar.123@gmail.com>
2014-06-20 23:54:56 -0700

    member_table changes
    column name mismatch corrected. all columns appended to the create
    table and alter table removed.

--- db/migrate/20140516022127_create_members.rb
+++ db/migrate/20140516022127_create_members.rb
@@ -3,8 +3,8 @@ class CreateMembers < ActiveRecord::Migration
     create_table :members do |t|
       t.string :email
       t.string :password_digest
-      t.string :f_name
-      t.string :l_name
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps
     end
--- db/migrate/20140517004700_add_member_name.rb
+++
@@ -1,5 +0,0 @@
-class AddMemberName < ActiveRecord::Migration
-  def change
-    add_column :f_name, :l_name, :string
-  end
-end
