82d6bb17a6065be659291ccbc7c8a124d89b60ee
SowmyaRam <sowmyar.123@gmail.com>
2014-06-20 23:30:56 -0700

    table column changes
    alter column changes added to create table schema

--- db/migrate/20140516022127_create_members.rb
+++ db/migrate/20140516022127_create_members.rb
@@ -3,6 +3,8 @@ class CreateMembers < ActiveRecord::Migration
     create_table :members do |t|
       t.string :email
       t.string :password_digest
+      t.string :f_name
+      t.string :l_name
 
       t.timestamps
     end
